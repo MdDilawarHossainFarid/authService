74d2ab671fffc23c451b3a0122cfcc5b65698a04
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-12-14 23:10:12 +0530

    server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res) => {
+    let sum = 1 + 2;
+    res.send(sum )
+})
+
+app.listen(port, () => {
+    console.log(`app is runing on port ${port}`)
+})
