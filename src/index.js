e99b1377c3aac2188784cbad68414a08e2f83f23
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-12-15 23:57:34 +0530

    database setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 
 const express = require('express');
+const { Sequelize } = require('sequelize');
 const app = express();
 const port = 3000;
+
+
+// Initialize Sequelize
+// const sequelize = new Sequelize({
+//   dialect: 'mysql',
+//   database: 'employee',
+//   user: 'root',
+//   password: '2023@#Node#D96',
+//   host: 'localhost',
+//   port: 3306,
+// });
+
+const sequelize = new Sequelize('employee', 'root', '2023@#Node#D96', {
+  host: 'localhost',
+  dialect:'mysql'})
+// Test connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('✅ Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('❌ Unable to connect to the database:', error);
+  });
 
 app.get('/',(req,res) => {
     let sum = 1 + 2;
